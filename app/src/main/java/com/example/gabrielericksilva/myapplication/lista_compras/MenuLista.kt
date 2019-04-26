71c609efd18f7a3170584e0fca7b68fe53d9029c
Gabriel Erick  Silva <gabrielericksilva@gmail.com>
2019-04-26 05:59:26 -0300

    Estudando os fundamentos da Liguagem Kotlin.

---
+++ app/src/main/java/com/example/gabrielericksilva/myapplication/lista_compras/MenuLista.kt
@@ -0,0 +1,13 @@
+package com.example.gabrielericksilva.myapplication.lista_compras
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.gabrielericksilva.myapplication.R
+
+class MenuLista : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_lista)
+    }
+}
